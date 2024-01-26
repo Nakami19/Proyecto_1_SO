ae2e5daa65976235157133590b4dbb2c21beb953
Nakami <natalia.rivas@correo.unimet.edu.ve>
2024-01-26 09:20:07 -0400

    :adhesive_bandage:

--- src/Clases/Developer.java
+++ src/Clases/Developer.java
@@ -16,18 +16,20 @@ public class Developer extends Thread {
     
     private int acc=0; //cuenta el dia, inicia en 0
     private Drive drive;
-    private int type; //tipo de trabajador 
+    private int type; //tipo de trabajo
     private int sueldoph; //sueldo por hora del trabajador 
     private Semaphore mutex; //semforo
     private int dayDuration;//duracion del dia 
     private float salarioacc=0; //salario acumulado en total, inicia en 0
+    private float produccionPordia; //tiempo que tarda un trabajador en crear su parte, cada trabajador puede tener un numero diferente que represente su dia, ej el animador es 0.34 y cuando pasan 3 dias hace algo, el guionista necesita dos dias entonces en vez de 0.34 puedo poner 0.5 
 
-    public Developer(Drive drive, int type, int sueldoph, Semaphore m, int dayDuration, float salarioacc) {
+    public Developer(Drive drive, int type, int sueldoph, Semaphore m, int dayDuration, float salarioacc, float ppd) {
         this.drive = drive;
         this.type = type;
         this.sueldoph = sueldoph;
         this.mutex = m;
         this.dayDuration = dayDuration;
+        this.produccionPordia=ppd;
     }
     
     
@@ -47,7 +49,7 @@ public class Developer extends Thread {
     }
     
     public void Work() {
-    
+        this.acc+=this.produccionPordia; 
     
     }
     
